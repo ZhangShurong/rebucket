b57d7e7af0c3b47e1c9170a88b4ec0fad0b12423
vergilzhang <vergilzhang@tencent.com>
2018-11-17 22:29:42 +0800

    test go

---
+++ rebucket.go
@@ -0,0 +1,27 @@
+// json.go
+package main
+ 
+import (
+    "encoding/json"
+    "fmt"
+)
+ 
+type Server struct {
+    ServerName string
+    ServerIP   string
+}
+ 
+type Serverslice struct {
+    Servers []Server
+}
+ 
+func main() {
+    var s Serverslice
+    str := `{"servers":[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},
+            {"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}`
+ 
+    json.Unmarshal([]byte(str), &s)
+    fmt.Println(s)
+    fmt.Println(s.Servers[0].ServerIP)
+ 
+}
