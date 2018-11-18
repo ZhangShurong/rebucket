8da60f2fe3f904e03516d81cdccb131656138f16
ZhangShurong <zhang_shurong@foxmail.com>
2018-11-18 19:09:23 +0800

    new

--- test.py
+++ test.py
@@ -1,24 +1,24 @@
-from multiprocessing import  Queue
-from threading import Thread
-import time
+import json
 import rebucket
+from rebucket import Stack
+from rebucket import Frame
 
-def read_stack(q):
-    for i in range(10):
-        q.put(i)
-        time.sleep(0.5)
-    print("end producer")
-
-def get_stack(q):
-    while 1:
-        stack = q.get()
-        print rebucket.query(stack)
+def read_dataset(json_path):
+    with open(json_path) as json_file:
+        dataset_dict = json.load(json_file)
+    all_stacks = []
+    for stack_dict in dataset_dict:
+        stack_id = stack_dict['stack_id']
+        duplicated_stack = stack_dict['duplicated_stack']
+        frame_arr = []
+        for fram_dict in stack_dict['stack_arr']:
+            frame = Frame(fram_dict)
+            frame_arr.append(frame)
+        all_stacks.append(Stack(stack_id, frame_arr, duplicated_stack))
+    return all_stacks
 def main():
-    q = Queue()
-    pro = Thread(target=read_stack, args=(q,))
-    cus = Thread(target=get_stack,args=(q,))
-    pro.start()
-    cus.start()
-    pass
+    json_path = 'dataset/eclipse/df_eclipse.json'
+    all_stacks = read_dataset(json_path)
+    print len(rebucket.clustering(all_stacks))
 if __name__ == "__main__":
     main()
